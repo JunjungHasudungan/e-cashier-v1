0a7aea707d7d09fbe1afb1441ad6a7bad9768af2
junjunghasudungan <junjunghasudungan43@gmail.com>
2025-12-07 08:36:49 +0700

    perubahan tampilan di cashier-dashboard bagian praktek

--- resources/views/cashier/example-index.blade.php
+++ resources/views/cashier/example-index.blade.php
@@ -10,7 +10,7 @@
                         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex duration-250 focus:outline focus:outline-2 focus:outline-red-500 w-full">
                             <div class="grid grid-cols-2 gap-2 w-full">
                                 {{-- @for ($i = 0; $i < 6; $i++) --}}
-                               <template x-if="products">
+                                <template x-if="products">
                                     <template x-for="product in products" :key="product.id">
                                         <div class="motion-safe:hover:scale-[1.01] transition-transform w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                                             <a href="#">
